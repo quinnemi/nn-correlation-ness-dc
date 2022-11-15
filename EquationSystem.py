dfeecb3a2bf16506d8218cbbb45dbb2c73bd2c6b
quinnemi <jufischer@uos.de>
2022-11-15 09:40:26 +0100

    syncing with pure_python branch

--- EquationSystem.py
+++ EquationSystem.py
@@ -1,6 +1,7 @@
 """
-Calculates the matrix K (K * kappa = h), c.f. notes on correlation function, eq. 25a)
+Functions to calculate the matrix K (K * kappa = h), c.f. notes on correlation function, eq. 25a)
 Used to calculate kappa_i which determine the conductivity
+as well as the resulting vector h
 
 Elements of K can be grouped into three cases:
 - K1: diagonal matrix elements (indices i,j refer to equal sites)
@@ -24,18 +25,19 @@ BETA = float(config['SYSTEM PARAMETERS']['beta'])
 MINJUMPBARRIER = float(config['SYSTEM PARAMETERS']['minJumpBarrier'])
 ATTEMPTFREQ = float(config['SYSTEM PARAMETERS']['attemptFreq'])
 
-#energyLandscape = EnergyLandscape(L, pattern="checker") # 3D matrix
-#jumpRates = energyLandscape.getJumpRates("CSP", ATTEMPTFREQ, BETA, minJumpBarrier=MINJUMPBARRIER) # 3D matrix
-#eqOccNum = energyLandscape.getEqOccupationNumbers(C, BETA) # 3D matrix
+energyLandscape = EnergyLandscape(L, pattern="checker") # 3D matrix
+jumpRates = energyLandscape.getJumpRates("CSP", ATTEMPTFREQ, BETA, minJumpBarrier=MINJUMPBARRIER) # 3D matrix
+eqOccNum = energyLandscape.getEqOccupationNumbers(C, BETA) # 3D matrix
 
-@njit(cache=True)
+idxList = []
+for z in range(L):
+    for y in range(L):
+        for x in range(L):
+            idxList.append((x,y,z))
+idxList = np.array(idxList)
+
+@njit
 def K() -> np.array:
-    idxList = []
-    for z in range(L):
-        for y in range(L):
-            for x in range(L):
-                idxList.append((x,y,z))
-    idxList = np.array(idxList)
 
     K = np.zeros((L**3, L**3))
     for i in idxList:
@@ -54,7 +56,7 @@ def K() -> np.array:
     
     return K
 
-@njit(cache=True)
+@njit
 def acc(array: np.array, tuple: tuple) -> float:
     """Access 3D array via 3-tuple"""
     i = tuple[0]
@@ -64,7 +66,7 @@ def acc(array: np.array, tuple: tuple) -> float:
     
     return a
 
-@njit(cache=True)
+@njit
 def K1(i: tuple) -> float:
     """Calculation of a single entry of K1 (i,j are equal sites)
         CSP model for jump rates is implied"""
@@ -83,7 +85,7 @@ def K1(i: tuple) -> float:
 
     return term1 + term2
 
-@njit(cache=True)
+@njit
 def K2(i: tuple, j: tuple) -> float:
     """Calculation of a single entry of K2 (i,j are nearest neighbors)
         CSP model for jump rates is implied"""
@@ -100,7 +102,7 @@ def K2(i: tuple, j: tuple) -> float:
 
     return term1 + term2 + term3
 
-@njit(cache=True)
+@njit
 def K3(i: tuple, j: tuple) -> float:
     """Calculation of a single entry of K2 (i,j are next nearest neighbors)
         CSP model for jump rates is implied"""
@@ -111,21 +113,52 @@ def K3(i: tuple, j: tuple) -> float:
 
     return term1
 
-@njit(cache=True)
-def D(pos1: tuple, pos2: tuple) -> float:
-    """Denominator term in eq. 17s
-        pos1: i, pos2: j"""
+@njit
+def D(i: tuple, j: tuple) -> float:
+    """Denominator term in eq. 17a"""
     
-    return 1/acc(eqOccNum, pos1) * (
-        sum([acc(jumpRates, p) for p in getNNidx(pos1) if not eqIdx(p, pos2)])
-    )   +  1/acc(eqOccNum, pos2) * (
-        sum([acc(jumpRates, p) for p in getNNidx(pos2) if eqIdx(p, pos1)])
+    return 1/acc(eqOccNum, i) * (
+        sum([acc(jumpRates, p) for p in getNNidx(i) if not eqIdx(p, j)])
+    )   +  1/acc(eqOccNum, j) * (
+        sum([acc(jumpRates, p) for p in getNNidx(j) if eqIdx(p, i)])
     )
+
+@njit
+def h() -> np.array:
+    """Returns resulting vector h. CSP model is implied."""
+    return np.array([hi(i) for i in idxList])
+
+@njit
+def hi(i: tuple) -> float:
+    """Returns entry h_i in h for site i. CSP model is implied"""
+
+    term1 = 0
+    for j in getNNidx(i):
+        term1 += b(j, i) * acc(jumpRates, j) * acc(eqOccNum, j)
+    term1 *= -(1-acc(eqOccNum, i))
+
+    term2 = 0
+    for j in getNNidx(i):
+        term2_1 = 0
+        for k in getNNidx(i):
+            if not eqIdx(k, j):
+                term2_1 += b(k, i) * acc(jumpRates, k) * acc(eqOccNum, k)
+        term2_1 *= (1-acc(eqOccNum, i)) * acc(eqOccNum, j)
+
+        term2_2 = 0
+        for l in getNNidx(j):
+            if not eqIdx(l, i):
+                term2_2 += b(l, j) * acc(jumpRates, l) * acc(eqOccNum, l)
+        term2_2 *= acc(eqOccNum, i) * (1-acc(eqOccNum, j))
+
+        term2 += (acc(jumpRates, j) - acc(jumpRates, i)) / D(i, j) * (term2_1 + term2_2)
+
+    return term1 + term2
 
 
 ############################## Supporting functions ##############################
 
-@njit(cache=True)
+@njit
 def eqIdx(i: tuple, j: tuple) -> bool:
     """Check indexes for equality"""
     res = [0, 0, 0]
@@ -134,13 +167,13 @@ def eqIdx(i: tuple, j: tuple) -> bool:
     
     return res[0] and res[1] and res[2]
 
-@njit(cache=True)
+@njit
 def pIdx(j: int) -> int:
         """Returns periodic index"""
         global L 
         return int(j % L)
 
-@njit(cache=True)
+@njit
 def getNN(a: np.array, i: tuple) -> np.array:
     """Returns entries of nearest neighbors in a periodic 3D array 'a' at position i"""
     
@@ -151,7 +184,7 @@ def getNN(a: np.array, i: tuple) -> np.array:
         a[pIdx(z-1)][y][x], a[pIdx(z+1)][y][x]
     ])
 
-@njit(cache=True)
+@njit
 def getNNidx(i: tuple) -> np.array:
     """Returns indexes of nearest neighbors in a periodic 3D array of size L at position i"""
     
@@ -165,7 +198,7 @@ def getNNidx(i: tuple) -> np.array:
         (x,y,pIdx(z-1)), (x,y,pIdx(z+1))
     ], dtype=np.int32)
 
-@njit(cache=True)
+@njit
 def getNNNidx(i: tuple) -> np.array:
     """Returns indexes of next nearest neighbors in a periodic 3D array of size L at position i"""
 
@@ -177,10 +210,6 @@ def getNNNidx(i: tuple) -> np.array:
             allNNN[idx] = nnOfnn
             idx += 1
 
-    #print(f'allNNN: {allNNN}')
-
-    #allNNN = np.array([getNNidx(j) for j in getNNidx(i)])
-    #allNNN = np.reshape(allNNN, (36, 3))
     filteredNNN = np.ones((18, 3), dtype=np.int32)*9999
     idxJ = 0
     for j in allNNN:
@@ -198,28 +227,21 @@ def getNNNidx(i: tuple) -> np.array:
                 idxJ += 1 
     return filteredNNN
 
-@njit(cache=True)
+@njit
 def tupleToIndex(t: tuple) -> int:
     return t[0] + t[1]*L + t[2]*L**2
 
-#print(timeit.timeit(K, number=100))
-#mat = K()
-#print(np.linalg.det(mat))
-#plt.matshow(mat)
-#plt.show()
+def testSupportFuncs():
+    a = np.array([[[1.,2.,3.],[4.,5.,6.],[7.,8.,9.]],
+                [[10.,11.,12.],[13.,14.,15.],[16.,17.,18.]],
+                [[19.,20.,21.],[22.,23.,24.],[25.,26.,27.]]])
+    i = (1,0,2)
+    j = (1,0,0)
+    k = (-3,4,5)
 
-#with open(Path(__file__).parent.parent.parent / "data" / "K_numba", 'w+') as file:
-#    file.write(str(mat))
-
-a = np.array([[[1.,2.,3.],[4.,5.,6.],[7.,8.,9.]],
-              [[10.,11.,12.],[13.,14.,15.],[16.,17.,18.]],
-              [[19.,20.,21.],[22.,23.,24.],[25.,26.,27.]]])
-i = (1,0,2)
-j = (1,0,0)
-k = (-3,4,5)
-print(f'acc: {a, i}')
-print(f'eqIdx: {eqIdx(i,i)}, {eqIdx(i,j)}')
-print(f'pIdx: {pIdx(k[0])}, {pIdx(k[1])}, {pIdx(k[2])}')
-print(f'getNNidx: {getNNidx(i)}')
-print(f'getNNNidx: {getNNNidx(i)}')
-print(f'tupleToIndex: {tupleToIndex(i)}')
+    print(f'acc: {a, i}')
+    print(f'eqIdx: {eqIdx(i,i)}, {eqIdx(i,j)}')
+    print(f'pIdx: {pIdx(k[0])}, {pIdx(k[1])}, {pIdx(k[2])}')
+    print(f'getNNidx: {getNNidx(i)}')
+    print(f'getNNNidx: {getNNNidx(i)}')
+    print(f'tupleToIndex: {tupleToIndex(i)}')
